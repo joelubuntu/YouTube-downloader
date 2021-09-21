fc76b3832d065fe986af3352bf28b15c9b0b9232
joelubuntu <72544077+joelubuntu@users.noreply.github.com>
2021-09-21 12:17:29 +0530

    Update ytdown.py

--- ytdown.py
+++ ytdown.py
@@ -1,35 +1,36 @@
 import os
 import datetime
 from pytube import YouTube
+from pytube import Playlist
 def video_download(link,res):
-        yt = YouTube(link)
-        title = yt.title
-        try:
-                if res.lower() == ("high"):
-                        reso_select = yt.streams.get_by_itag(22)
-                elif res.lower() == ("low"):
-                        reso_select = yt.streams.get_by_itag(17)
-                elif res.lower() == ('medium'):
-                        reso_select = yt.streams.get_by_itag(18)
-                reso_select.download()
-                print("Downloaded " , title,"\n")
-        except:
-                print("Enter vaild input!",'\n')
+	yt = YouTube(link)
+	title = yt.title
+	try:
+		if res.lower() == ("high"):
+			reso_select = yt.streams.get_by_itag(22)
+		elif res.lower() == ("low"):
+			reso_select = yt.streams.get_by_itag(17)
+		elif res.lower() == ('medium'):
+			reso_select = yt.streams.get_by_itag(18)
+		reso_select.download()
+		print("Downloaded " , title,"\n")
+	except:
+		print("Enter vaild input!",'\n')
 
 def audio_download(link):
-        yt = YouTube(link)
-        title = yt.title
-        try:
-                try:
-                        stream = yt.streams.get_by_itag(140)
-                        stream.download()
-                        print("Downloaded " , title ,'\n')
-                except:
-                        stream = yt.streams.get_by_itag(139)
-                        stream.download()
-                        print("Downloaded " , title ,'\n' )
-        except:
-                print("Failed!", '\n')
+	yt = YouTube(link)
+	title = yt.title
+	try:
+		try:
+			stream = yt.streams.get_by_itag(140)
+			stream.download()
+			print("Downloaded " , title ,'\n')
+		except:
+			stream = yt.streams.get_by_itag(139)
+			stream.download()
+			print("Downloaded " , title ,'\n' )
+	except:
+		print("Failed!", '\n')
 
 def welcome():
     user_name = os.getlogin()
@@ -45,26 +46,41 @@ def welcome():
 exit = False
 
 def help():
-        print("\n")
 	print('For downloading video:',"\n",'use this command : video (link)',"\n",'example: video https://www.youtube.com/watch?v=dQw4w9WgXcQ',"\n",'resolution : low = 144p , medium = 360p , high = 720p',"\n")
 	print('For downloading whole playlist:',"\n",'use this command : playlist (link of playlist) (no. of video playlist have)',"\n")
 	print('For downloading audio:',"\n",'use this command : audio (link)',"\n",'ex: audio https://www.youtube.com/watch?v=dQw4w9WgXcQ',"\n")
 
+def playlist(link):
+	res = input('Enter resolution: ')
+	if res.lower() == ("high"):
+		itag = 22
+	elif res.lower() == ("low"):
+		itag = 17
+	elif res.lower() == ('medium'):
+		itag = 18
+
+	play = Playlist(link)
+	for video in play.videos:
+		video.streams.get_by_itag(itag).download()
+	print('Downloaded! ')
+
 def menu():
-        exit = False
-        while exit == False:
-                user_cmd = input('Enter your command: ').split(' ')
-                if user_cmd[0].lower() == ("video"):
-                        res = input('Enter resolution: ')
-                        video_download(user_cmd[1],res)
-                elif user_cmd[0].lower() == ("audio"):
-                        audio_download(user_cmd[1])
-                elif user_cmd[0].lower() == ('help'):
-                        help()
-                elif user_cmd[0].lower() == ('exit') or user_cmd[0].lower() == ('quit'):
-                        exit = True
-                else:
-                        print("Invaild input! ")
+	exit = False
+	while exit == False:
+		user_cmd = input('Enter your command: ').split(' ')
+		if user_cmd[0].lower() == ("video"):
+			res = input('Enter resolution: ')
+			video_download(user_cmd[1],res)
+		elif user_cmd[0].lower() == ("audio"):
+			audio_download(user_cmd[1])
+		elif user_cmd[0].lower() == ('help'):
+			help()
+		elif user_cmd[0].lower() == ('exit') or user_cmd[0].lower() == ('quit'):
+			exit = True
+		elif user_cmd[0].lower() == ("playlist"):
+			playlist(user_cmd[1])
+		else:
+			print("Invaild input! ")
 
 welcome()
 menu()
