36d090f07d26782a720c0793894ba081bc868743
Shreyash <52196231+bubaneshreyash3@users.noreply.github.com>
2021-10-02 09:31:24 +0530

    added download playlist as audio

--- ytdown.py
+++ ytdown.py
@@ -40,8 +40,25 @@ def audio_download(link, quality):
     except:
         print("Failed , Try different quality audio")
 
+def playlist_audio(link, res):
+    try:
+        if res.lower() == ("high"):
+            itag = 251
+        elif res.lower() == ("low"):
+            itag = 249
+        elif res.lower() == ('medium'):
+            itag = 50
+        play = Playlist(link)
+        print("Initiating playlist download")
+        for video in play.videos:
+            video.streams.filter(
+                file_extension="mp3").get_by_itag(itag).download()
+        print('Downloaded! ')
+    except:
+        print("Failed, try diffrent resolution.")
 
-def playlist(link, res):
+
+def playlist_video(link, res):
     try:
         if res.lower() == ("high"):
             itag = 22
@@ -72,26 +89,31 @@ def welcome():
 
 
 def help():
-    print("\nUsage: python3 " +
-          sys.argv[0] + " <video/audio> [URL ..] <resolution/quality>\n")
-    print('Example to download video - \n    python3 ',
-          sys.argv[0], ' video https://www.youtube.com/watch?v=dQw4w9WgXcQ high\n')
-    print('Example to download audio - \n    python3 ',
-          sys.argv[0], ' audio https://www.youtube.com/watch?v=dQw4w9WgXcQ medium\n')
-    print('Example to download playlist - \n    python3 ',
-          sys.argv[0], ' playlist https://www.youtube.com/watch?v=dQw4w9WgXcQ high\n')
+    print(f'''Usage: python3 {sys.argv[0]} + " <video/audio> [URL ..] <resolution/quality> [mp3/mp4]\n")
+Examples:
+To download video -
+python3 {sys.argv[0]} video https://www.youtube.com/watch?v=dQw4w9WgXcQ high\n
+To download audio -
+python3 {sys.argv[0]} audio https://www.youtube.com/watch?v=dQw4w9WgXcQ medium\n
+To download playlist as video-
+python3 {sys.argv[0]} playlist video https://www.youtube.com/playlist?list=PL6rBC_87VKs76V9Vc0s44Gg_Q6qObZrFY high''')
 
 
 def menu():
-    if len(sys.argv) > 4 or len(sys.argv) < 4:
+    if not 4 <= len(sys.argv) <= 5:
         help()
-    elif sys.argv[1] == 'video':
+    elif sys.argv[1].lower() == 'video':
         video_download(sys.argv[2], sys.argv[3])
-    elif sys.argv[1] == 'audio':
+    elif sys.argv[1].lower() == 'audio':
         audio_download(sys.argv[2], sys.argv[3])
-    elif sys.argv[1] == "playlist":
-        playlist(sys.argv[2], sys.argv[3])
-
+    elif sys.argv[1].lower() == "playlist":
+        if sys.argv[2].lower() == 'video':
+            playlist_video(sys.argv[3], sys.argv[4])
+        if sys.argv[2].lower() == 'audio':
+            playlist_audio(sys.argv[3], sys.argv[4])
+    else:
+        print('Invalid option!')
+        exit()
 
 welcome()
 menu()
